c81c6851953277dc98587c4c086070e3c5a4b08f
peti446 <peti446@gmail.com>
2018-10-14 19:27:41 +0100

    Coroutine Helper

---
+++ Assets/Scripts/Helpers/CoroutineUtilities.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+public static class CoroutineUtilities
+{
+
+
+    ///<summary>
+    ///Coroutine function wrapper for a function to be executed after a delay
+    ///</summary>
+    ///<param name="action">
+    ///The action in question, can be lambda, delegates, a functioncall itself...
+    ///</param>
+    ///<param name="delay">
+    ///Time delay to execute the function in seconds
+    ///</param>
+    ///<example>
+    ///StartCoroutine(CoroutineUtils.DelaySeconds(() => DebugUtils.Log("executed after 2 seconds"), 2);
+    ///</example>
+    public static IEnumerator DelaySeconds(Action action, float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        action();
+    }
+
+    ///<summary>
+    ///Coroutine function wrapper that will wait for a couple of seconds
+    ///</summary>
+    ///<param name="time">
+    ///Time delay 
+    ///</param>
+    ///<example>
+    ///StartCoroutine(CoroutineUtils.WaitForSeconds(5));
+    ///</example>
+    public static IEnumerator WaitForSeconds(float time)
+    {
+        yield return new WaitForSeconds(time);
+    }
+
+
+    ///<summary>
+    ///Coroutine function wrapper for a function to be executed in a corroutine
+    ///</summary>
+    ///<param name="action">
+    ///The action in question, can be lambda, delegates, a functioncall itself...
+    ///</param>
+    ///<example>
+    ///StartCoroutine(CoroutineUtils.DelaySeconds(() => Debug.Log("executed in a corroutine"));
+    ///</example>
+    public static IEnumerator Do(Action action)
+    {
+        action();
+        yield return 0;
+    }
+
+}
