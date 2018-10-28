d53586ff8725d10534aa47513900f1fa489c555b
kony <platinum.neet@gmail.com>
2018-10-28 01:40:47 +0100

    gamemanager singleton type base file

---
+++ Assets/Scripts/Gameplay/GameManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum EGameState  
+{
+  InGame, InMenu, LoadingGame, InLobby, OnPauseScreen
+};
+
+
+
+public class GameManager : MonoBehaviour
+{
+
+  public static GameManager instance = null;
+
+  private void Awake()
+  {
+    if (instance == null)
+      instance = this;
+
+    else if (instance != this)
+      Destroy(gameObject);
+
+    DontDestroyOnLoad(this);
+
+    //initialise - game start loading calling 
+  }
+
+
+}
