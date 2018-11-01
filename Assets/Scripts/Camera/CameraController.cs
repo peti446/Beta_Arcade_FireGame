d34bbd1d4b2c3cfa56d9ee088527733faa50fafd
kesada7 <kesadacastillero@gmail.com>
2018-11-01 01:16:00 +0000

    Changes to player controller, now with camera

---
+++ Assets/Scripts/Camera/CameraController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+    
+    [SerializeField]
+    float camSensY = 0.25f; //How sensitive it with mouse
+    [SerializeField]
+    float camSensX = 0.0f; //How sensitive it with mouse
+
+    private Vector3 lastMouse = new Vector3(255, 255, 255); //kind of in the middle of the screen, rather than at the top (play)
+
+
+    void Update()
+    {
+
+
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            camSensX = 0.5f;
+           
+
+        }
+        else
+        {
+            camSensX = 0.0f;
+            // transform.eulerAngles = new Vector3(0, 0, 0);
+
+            // transform.rotation = Quaternion.Euler(0, 0, 0); // this is 90 degrees around y axis
+            //Debug.Log(transform.rotation.y);
+            //if(transform.rotation.y > 0)
+            //{
+            //    transform.Rotate(Vector3.right * 10, Time.deltaTime, Space.World);
+            //}
+
+        }
+        lastMouse = Input.mousePosition - lastMouse;
+        lastMouse = new Vector3(-lastMouse.y * camSensY, lastMouse.x * camSensX, 0);
+        lastMouse = new Vector3(transform.eulerAngles.x + lastMouse.x, transform.eulerAngles.y + lastMouse.y, 0);
+        transform.eulerAngles = lastMouse;
+        lastMouse = Input.mousePosition;
+        
+
+
+
+
+    }
+
+
+
+}
--- Assets/Scripts/Player/PlayerInputs.cs
+++ Assets/Scripts/Player/PlayerInputs.cs
@@ -4,7 +4,18 @@ using UnityEngine;
 
 [RequireComponent(typeof(Character))]
 public class PlayerInputs : MonoBehaviour {
-       
+
+    [SerializeField]
+    private float playerSpeed;
+
+    [SerializeField]
+    float camSensY = 0.0f; //How sensitive it with mouse
+    [SerializeField]
+    float camSensX = 0.5f; //How sensitive it with mouse
+
+    private Vector3 lastMouse = new Vector3(255, 255, 255); //kind of in the middle of the screen, rather than at the top (play)
+
+
     private Character m_character;
     
     private Rigidbody playerRigibody;
@@ -41,7 +52,21 @@ public class PlayerInputs : MonoBehaviour {
         ////playerRigibody.AddRelativeForce(horizontalInput * playerSpeed, 0, 0,  ForceMode.Force);
         //transform.Rotate(0, playerTurn * horizontalInput, 0);
 
-        m_character.MovePlayer(verticalInput, horizontalInput);
+        //m_character.MovePlayer(verticalInput, horizontalInput);
+
+
+
+        if(verticalInput != 0 || horizontalInput != 0)
+        {
+            playerRigibody.velocity = (gameObject.transform.forward * verticalInput * playerSpeed) + (gameObject.transform.right * horizontalInput * playerSpeed);
+
+        }
+        lastMouse = Input.mousePosition - lastMouse;
+        lastMouse = new Vector3(-lastMouse.y * camSensY, lastMouse.x * camSensX, 0);
+        lastMouse = new Vector3(transform.eulerAngles.x + lastMouse.x, transform.eulerAngles.y + lastMouse.y, 0);
+        transform.eulerAngles = lastMouse;
+        lastMouse = Input.mousePosition;
+
 
 
     }
@@ -49,10 +74,10 @@ public class PlayerInputs : MonoBehaviour {
     protected void LateUpdate()
     {
         //TODO call the character function instead of doing it directly here
-        transform.localEulerAngles = new Vector3(0, transform.localEulerAngles.y, 0);
-        if (verticalInput == 0)
-        {
-            playerRigibody.velocity = new Vector3(playerRigibody.velocity.x * 0.80f, playerRigibody.velocity.y * 0.80f, playerRigibody.velocity.z * 0.80f);
-        }
+        //transform.localEulerAngles = new Vector3(0, transform.localEulerAngles.y, 0);
+        //if (verticalInput == 0)
+        //{
+        //    playerRigibody.velocity = new Vector3(playerRigibody.velocity.x * 0.80f, playerRigibody.velocity.y * 0.80f, playerRigibody.velocity.z * 0.80f);
+        //}
     }
 }
