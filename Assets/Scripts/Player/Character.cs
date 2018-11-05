eaf26e549ca6246ed36d01b5fe30591daa3caa8e
kony <platinum.neet@gmail.com>
2018-11-05 01:21:41 +0000

    character toggling of the hose prefab to use as player's weapon

--- Assets/Scripts/Player/Character.cs
+++ Assets/Scripts/Player/Character.cs
@@ -2,63 +2,75 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Character : MonoBehaviour {
+public class Character : MonoBehaviour
+{
 
-    private Rigidbody playerRigibody;
+  private Rigidbody playerRigibody;
 
-    [SerializeField]
-    private float playerSpeed;
-    [SerializeField]
-    private float playerTurn;
+  [SerializeField]
+  private float playerSpeed;
+  [SerializeField]
+  private float playerTurn;
+
+  [SerializeField]
+  private ParticleSystem hoseWater;
 
 
-    private void Awake()
+  private void Awake()
+  {
+    playerRigibody = GetComponent<Rigidbody>();
+  }
+  // Use this for initialization
+
+  private int Health;
+  private bool IsAlive;
+  void Start()
+  {
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+
+  }
+
+  public void MovePlayer(float verticalInput, float horizontalInput)
+  {
+    if (verticalInput == 0 && horizontalInput == 0)
+      return;
+    //playerRigibody.AddRelativeForce(0, 0, verticalInput * playerSpeed, ForceMode.Force);
+    //playerRigibody.AddRelativeForce(horizontalInput * playerSpeed, 0, 0,  ForceMode.Force);
+    //transform.Rotate(0, playerTurn * horizontalInput, 0);
+    if (verticalInput == 1)
+      playerRigibody.velocity = gameObject.transform.forward * playerSpeed;
+    else if (verticalInput == 0)
+      playerRigibody.velocity = new Vector3(0, 0, 0);
+    else
+      playerRigibody.velocity = gameObject.transform.forward * -playerSpeed;
+  }
+
+  public void ToggleHose(bool open)
+  {
+    if (open)
     {
-          playerRigibody = GetComponent<Rigidbody>();
+      hoseWater.enableEmission = true;
+      hoseWater.Play();
     }
-    // Use this for initialization
-
-    private int Health;
-    private bool IsAlive;
-    void Start()
+    else if (!open)
     {
+      hoseWater.enableEmission = false;
     }
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
-
-    public void MovePlayer(float verticalInput, float horizontalInput)
-    {
-        if (verticalInput == 0 && horizontalInput == 0)
-            return;
-        //playerRigibody.AddRelativeForce(0, 0, verticalInput * playerSpeed, ForceMode.Force);
-        //playerRigibody.AddRelativeForce(horizontalInput * playerSpeed, 0, 0,  ForceMode.Force);
-        //transform.Rotate(0, playerTurn * horizontalInput, 0);
-        if (verticalInput == 1)
-            playerRigibody.velocity = gameObject.transform.forward * playerSpeed;
-        else if (verticalInput == 0)
-            playerRigibody.velocity = new Vector3(0, 0, 0);
-        else
-            playerRigibody.velocity = gameObject.transform.forward * -playerSpeed;
-    }
+  }
 
 
-    /// <summary>
-    /// Sets the direction of the characters
-    /// </summary>
-    /// <param name="newDir"><c>Vector3</c> The new direciton of this characters</param>
-    public void SetDir(Vector3 newDir)
-    {
-        
+  /// <summary>
+  /// Sets the direction of the characters
+  /// </summary>
+  /// <param name="newDir"><c>Vector3</c> The new direciton of this characters</param>
+  public void SetDir(Vector3 newDir)
+  {
 
-    }
-    void ToggleHose()
-    {
 
-    }
-
-    
+  }
 
 }
--- Assets/Scripts/Player/PlayerInputs.cs
+++ Assets/Scripts/Player/PlayerInputs.cs
@@ -3,78 +3,88 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(Character))]
-public class PlayerInputs : MonoBehaviour {
+public class PlayerInputs : MonoBehaviour
+{
 
-    [SerializeField]
-    private float playerSpeed;
+  [SerializeField]
+  private float playerSpeed;
 
-    [SerializeField]
-    float camSensY = 0.0f; //How sensitive it with mouse
-    [SerializeField]
-    float camSensX = 0.5f; //How sensitive it with mouse
+  [SerializeField]
+  float camSensY = 0.0f; //How sensitive it with mouse
+  [SerializeField]
+  float camSensX = 0.5f; //How sensitive it with mouse
 
-    private Vector3 lastMouse = new Vector3(255, 255, 255); //kind of in the middle of the screen, rather than at the top (play)
+  private Vector3 lastMouse = new Vector3(255, 255, 255); //kind of in the middle of the screen, rather than at the top (play)
+
+  [SerializeField]
+  private Character m_character;
+
+  private Rigidbody playerRigibody;
+
+  private float horizontalInput;
+  private float verticalInput;
 
 
-    private Character m_character;
+  // Use this for initialization
+  private void Awake()
+  {
+    m_character = GetComponent<Character>();
+    playerRigibody = GetComponent<Rigidbody>();
+  }
+  void Start()
+  {
+
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+
+    horizontalInput = Input.GetAxis("Horizontal");
+    verticalInput = Input.GetAxis("Vertical");
     
-    private Rigidbody playerRigibody;
-
-    private float horizontalInput;
-    private float verticalInput;
-
-
-    // Use this for initialization
-    private void Awake()
+    if(Input.GetKeyDown(KeyCode.E))
     {
-        m_character = GetComponent<Character>();
-        playerRigibody = GetComponent<Rigidbody>();
+      //building.gameObject.GetComponent<BuildingStatus>().Extinguish();
+      m_character.ToggleHose(true);
     }
-    void Start()
+    if (Input.GetKeyUp(KeyCode.E))
     {
-      
+      m_character.ToggleHose(false);
     }
 
-    // Update is called once per frame
-    void Update()
+
+  }
+
+  private void FixedUpdate()
+  {
+
+    //m_character.MovePlayer(verticalInput, horizontalInput);
+
+
+    if (verticalInput != 0 || horizontalInput != 0)
     {
-
-        horizontalInput = Input.GetAxis("Horizontal");
-        verticalInput = Input.GetAxis("Vertical");
-
-
+      playerRigibody.velocity = (gameObject.transform.forward * verticalInput * playerSpeed) + (gameObject.transform.right * horizontalInput * playerSpeed);
 
     }
 
-    private void FixedUpdate()
-    {
-
-        //m_character.MovePlayer(verticalInput, horizontalInput);
-        
-
-        if(verticalInput != 0 || horizontalInput != 0)
-        {
-            playerRigibody.velocity = (gameObject.transform.forward * verticalInput * playerSpeed) + (gameObject.transform.right * horizontalInput * playerSpeed);
-
-        }
-
-        lastMouse = Input.mousePosition - lastMouse;
-        lastMouse = new Vector3(-lastMouse.y * camSensY, lastMouse.x * camSensX, 0);
-        lastMouse = new Vector3(transform.eulerAngles.x + lastMouse.x, transform.eulerAngles.y + lastMouse.y, 0);
-        transform.eulerAngles = lastMouse;
-        lastMouse = Input.mousePosition;
+    lastMouse = Input.mousePosition - lastMouse;
+    lastMouse = new Vector3(-lastMouse.y * camSensY, lastMouse.x * camSensX, 0);
+    lastMouse = new Vector3(transform.eulerAngles.x + lastMouse.x, transform.eulerAngles.y + lastMouse.y, 0);
+    transform.eulerAngles = lastMouse;
+    lastMouse = Input.mousePosition;
 
 
 
-    }
+  }
 
-    //protected void LateUpdate()
-    //{
-    //    //TODO call the character function instead of doing it directly here
-    //    //transform.localEulerAngles = new Vector3(0, transform.localEulerAngles.y, 0);
-    //    //if (verticalInput == 0)
-    //    //{
-    //    //    playerRigibody.velocity = new Vector3(playerRigibody.velocity.x * 0.80f, playerRigibody.velocity.y * 0.80f, playerRigibody.velocity.z * 0.80f);
-    //    //}
-    //}
+  //protected void LateUpdate()
+  //{
+  //    //TODO call the character function instead of doing it directly here
+  //    //transform.localEulerAngles = new Vector3(0, transform.localEulerAngles.y, 0);
+  //    //if (verticalInput == 0)
+  //    //{
+  //    //    playerRigibody.velocity = new Vector3(playerRigibody.velocity.x * 0.80f, playerRigibody.velocity.y * 0.80f, playerRigibody.velocity.z * 0.80f);
+  //    //}
+  //}
 }
