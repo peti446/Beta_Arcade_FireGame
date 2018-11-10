f09be9085c5068cd822d625c33518e5e7a3f62f3
LaraCM <lcm.briek@gmail.com>
2018-11-10 17:58:01 +0000

    Test ilumination and models added

---
+++ Assets/Scripts/Technical Art/Material_Change.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Material_Change : MonoBehaviour {
+
+    [Range(0,4)]
+    public int CurrentMaterial;
+    public List<Material> Materials;
+    int tempint;
+
+
+    private void OnDrawGizmos()
+    {
+        if (tempint != CurrentMaterial) ChangeMaterial();
+    }
+
+    void ChangeMaterial()
+    {
+        if (CurrentMaterial >= Materials.Count) { Debug.Log("You dont have that many textures bro"); CurrentMaterial = 0; return; }
+        this.gameObject.GetComponent<Renderer>().material = Materials[CurrentMaterial];
+        tempint = CurrentMaterial;
+    }
+}
