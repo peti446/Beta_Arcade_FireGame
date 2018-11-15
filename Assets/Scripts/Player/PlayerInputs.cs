5fbb6e1a5bca898908569a929efc9255200e81ba
LaraCM <lcm.briek@gmail.com>
2018-11-15 18:09:52 +0000

    Added new models, modified for multiple texture editing of the models, changed lighting

--- Assets/Scripts/Player/PlayerInputs.cs
+++ Assets/Scripts/Player/PlayerInputs.cs
@@ -5,79 +5,79 @@ using UnityEngine;
 [RequireComponent(typeof(Character))]
 public class PlayerInputs : MonoBehaviour
 {
-  
-  //[SerializeField]
-  //float camSensY = 0.0f; //How sensitive it with mouse
-  //[SerializeField]
-  //float camSensX = 0.5f; //How sensitive it with mouse
 
-  //private Vector3 lastMouse = new Vector3(255, 255, 255); //kind of in the middle of the screen, rather than at the top (play)
+    //[SerializeField]
+    //float camSensY = 0.0f; //How sensitive it with mouse
+    //[SerializeField]
+    //float camSensX = 0.5f; //How sensitive it with mouse
 
-  [SerializeField]
-  private Character m_character;
+    //private Vector3 lastMouse = new Vector3(255, 255, 255); //kind of in the middle of the screen, rather than at the top (play)
 
-  private Rigidbody playerRigibody;
+    [SerializeField]
+    private Character m_character;
 
-  private float horizontalInput;
-  private float verticalInput;
+    private Rigidbody playerRigibody;
 
-
-  // Use this for initialization
-  private void Awake()
-  {
-    m_character = GetComponent<Character>();
-    playerRigibody = GetComponent<Rigidbody>();
-  }
-  void Start()
-  {
-
-  }
-
-  // Update is called once per frame
-  void Update()
-  {
-
-    horizontalInput = Input.GetAxis("Horizontal");
-    verticalInput = Input.GetAxis("Vertical");
+    private float horizontalInput;
+    private float verticalInput;
 
 
-    
-    if (Input.GetKeyDown(KeyCode.E))
+    // Use this for initialization
+    private void Awake()
     {
-      m_character.ToggleHose(true);
+        m_character = GetComponent<Character>();
+        playerRigibody = GetComponent<Rigidbody>();
+    }
+    void Start()
+    {
+
     }
 
-    if (Input.GetKey(KeyCode.E))
+    // Update is called once per frame
+    void Update()
     {
-      m_character.InteractRay();
+
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+
+
+
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            m_character.ToggleHose(true);
+        }
+
+        if (Input.GetKey(KeyCode.E))
+        {
+            m_character.InteractRay();
+        }
+
+        if (Input.GetKeyUp(KeyCode.E))
+        {
+            m_character.ToggleHose(false);
+        }
+
+
     }
 
-    if (Input.GetKeyUp(KeyCode.E))
+    private void FixedUpdate()
     {
-      m_character.ToggleHose(false);
+
+        //m_character.MovePlayer(verticalInput, horizontalInput);
+
+
+        //  if (verticalInput != 0 || horizontalInput != 0)
+        //  {
+        //    playerRigibody.velocity = (gameObject.transform.forward * verticalInput * playerSpeed) + (gameObject.transform.right * horizontalInput * playerSpeed);
+
+        //  }
+
+        //  lastMouse = Input.mousePosition - lastMouse;
+        //  lastMouse = new Vector3(-lastMouse.y * camSensY, lastMouse.x * camSensX, 0);
+        //  lastMouse = new Vector3(transform.eulerAngles.x + lastMouse.x, transform.eulerAngles.y + lastMouse.y, 0);
+        //  transform.eulerAngles = lastMouse;
+        //  lastMouse = Input.mousePosition;
+
+        //}
     }
-
-
-  }
-
-  private void FixedUpdate()
-  {
-
-    //m_character.MovePlayer(verticalInput, horizontalInput);
-
-
-  //  if (verticalInput != 0 || horizontalInput != 0)
-  //  {
-  //    playerRigibody.velocity = (gameObject.transform.forward * verticalInput * playerSpeed) + (gameObject.transform.right * horizontalInput * playerSpeed);
-
-  //  }
-
-  //  lastMouse = Input.mousePosition - lastMouse;
-  //  lastMouse = new Vector3(-lastMouse.y * camSensY, lastMouse.x * camSensX, 0);
-  //  lastMouse = new Vector3(transform.eulerAngles.x + lastMouse.x, transform.eulerAngles.y + lastMouse.y, 0);
-  //  transform.eulerAngles = lastMouse;
-  //  lastMouse = Input.mousePosition;
-
-  //}
-
 }
--- Assets/Scripts/Technical Art/Material_Change.cs
+++ Assets/Scripts/Technical Art/Material_Change.cs
@@ -4,21 +4,48 @@ using UnityEngine;
 
 public class Material_Change : MonoBehaviour {
 
-    [Range(0,4)]
+    [Range(0, 4)]
     public int CurrentMaterial;
+    public bool isComplex = false;
     public List<Material> Materials;
     int tempint;
 
 
     private void OnDrawGizmos()
     {
-        if (tempint != CurrentMaterial) ChangeMaterial();
+        if (tempint != CurrentMaterial)
+        {
+            if (CurrentMaterial >= Materials.Count) { Debug.Log("You dont have that many textures bro"); CurrentMaterial = 0; }
+            else { if (!isComplex) {ChangeMaterial(); } else ChangeComplexMaterials(); }
+        }
     }
 
     void ChangeMaterial()
     {
-        if (CurrentMaterial >= Materials.Count) { Debug.Log("You dont have that many textures bro"); CurrentMaterial = 0; return; }
         this.gameObject.GetComponent<Renderer>().material = Materials[CurrentMaterial];
         tempint = CurrentMaterial;
     }
+
+    void ChangeComplexMaterials()
+    {
+        Renderer[] children;
+        children = GetComponentsInChildren<Renderer>();
+
+        foreach (Renderer rend in children)
+        {
+            rend.material= Materials[CurrentMaterial];
+            /*var mats = new Material[rend.sharedMaterials.Length];
+            for (int j = 0; j < rend.sharedMaterials.Length; j++)
+            {
+                mats[j] = Materials[CurrentMaterial];
+
+            }
+            rend.sharedMaterials = mats;*/
+        }
+        tempint = CurrentMaterial;
+    }
 }
+
+
+
+         
