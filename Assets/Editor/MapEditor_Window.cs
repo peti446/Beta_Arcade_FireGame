328b9e2b3d1bd679e07c29c7b2b068d841eab690
peti446 <peti446@gmail.com>
2018-11-22 01:45:34 +0000

    Started custom window

---
+++ Assets/Editor/MapEditor_Window.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class MapEditor_Window : EditorWindow {
+
+	[MenuItem("Custom tools/Map Tile Controller")]
+	public static void CreateWindow()
+	{
+		GetWindow<MapEditor_Window>("Map Tile Controller");
+	}
+}
