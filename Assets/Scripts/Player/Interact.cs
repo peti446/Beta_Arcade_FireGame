af3e13fae553b6223e3c1cebf435d7ba82ecb5e2
kony <platinum.neet@gmail.com>
2018-11-27 12:30:46 +0000

    class script to add to any interactuable object

---
+++ Assets/Scripts/Player/Interact.cs
@@ -0,0 +1,43 @@
+﻿/*
+   .
+   |\      ,--.
+ . \ `\   /  _ \__
+ |\_\  `-'   "  |
+ |   \          |
+ \    `.        |
+  \     `--    /
+   \          /
+    `,-.____/
+  _ / |
+   |  |
+     /| 
+     kony pyony was here
+ */
+
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Interact : MonoBehaviour {
+  public UnityEvent<Character> ClientInteraction;
+  public UnityEvent<Character> ServerInteraction;
+
+
+  public void ClientInteract (Character character)
+  {
+    if(MainNetworkManager._instance.LocalPlayer.ID == character.ControllingPlayerID && ClientInteraction != null)
+    {
+      ClientInteraction.Invoke(character);
+    }
+  }
+
+  public void ServerInteract(Character character)
+  {
+    if (MainNetworkManager.Is_Server && ServerInteraction != null)
+    {
+      ServerInteraction.Invoke(character);
+    }
+  }
+}
