d6ffc120a4fe0d98f17c1cda3a25647d155c089e
kony <platinum.neet@gmail.com>
2018-12-02 22:55:47 +0000

    added server button for GUI debugging interaction

--- Assets/Editor/Custom_Inspectors/CI_Interact.cs
+++ Assets/Editor/Custom_Inspectors/CI_Interact.cs
@@ -3,6 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
 
+
+/// <summary>
+/// the script that we're customising is the Interact script - 
+/// this means that the interact script will now have custom GUI properties
+/// for easier debugging.
+/// </summary>
 [CustomEditor(typeof(Interact))]
 public class CI_Interact : Editor {
 
@@ -14,13 +20,24 @@ public class CI_Interact : Editor {
     
   }
 
+  /// <summary>
+  /// custom inpsector for an specific object class
+  /// overriding is necessary in order for it to work.
+  /// </summary>
   public override void OnInspectorGUI()
   {
     source = (Character)EditorGUILayout.ObjectField(source, typeof(Character), true);
+    
     //TODO Kony: add security check that the source object is on scene 
-    if (GUILayout.Button("aaa") && source != null)
+
+    if (GUILayout.Button("client side interact") && source != null)
     {
       ((Interact)(target)).ClientInteract(source);
+    }
+
+    else if (GUILayout.Button("server side interact") && source != null)
+    {
+      ((Interact)(target)).ServerInteract(source);
     }
   }
 }
