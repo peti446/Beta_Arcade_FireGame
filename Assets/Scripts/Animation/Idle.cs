0e45393891eeb45ca49a353746a591bd1fbb680f
WINDOWS\q5122113 <benshoesmith96@gmail.com>
2019-01-08 16:27:55 +0000

    no message

---
+++ Assets/Scripts/Animation/Idle.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Idle : MonoBehaviour {
+    
+    public Animator anim;
+    public GameObject animHolder;
+
+
+	// Use this for initialization
+	void Start ()
+    {
+        //anim = gameObject.GetComponent<Animator>();
+        //anim = animHolder.GetComponent<Animator>();
+        
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		if(Input.GetKeyDown(KeyCode.K))
+        {
+            Debug.Log("Working");
+            anim.Play("Idle");
+        }
+	}
+}
